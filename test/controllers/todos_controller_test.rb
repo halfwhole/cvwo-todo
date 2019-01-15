305bd1af1f0c406373c059af2572188150e2dcac
halfwhole <limziwei2@gmail.com>
2019-01-15 10:49:52 +0800

    Add some tests for Todo model and controller

--- test/controllers/todos_controller_test.rb
+++ test/controllers/todos_controller_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class TodosControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @todo = Todo.new(content: 'test content')
+    @todo.save
+  end
+
+  test "should get todos" do
+    get todos_path
+    assert_response :success
+  end
+
+  test "should create todo" do
+    assert_difference('Todo.count') do
+      post todos_path, params: { todo: { content: 'new content' } }
+    end
+    assert_redirected_to todos_path
+  end
+
+  test "should not create todo" do
+    assert_no_difference('Todo.count') do
+      post todos_path, params: { todo: { content: '   ' } }
+    end
+    assert_redirected_to todos_path
+  end
+
+  test "should update todo" do
+    patch todo_path(@todo), params: { todo: { content: 'updated content' } }
+    assert_redirected_to todos_path
+    @todo.reload
+    assert_equal 'updated content', @todo.content
+  end
+
 end
--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class TodoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @todo1 = Todo.new
+    @todo2 = Todo.new(content: "   ")
+    @todo3 = Todo.new(content: "todo2 content")
+  end
+
+  test "should be valid" do
+    assert @todo3.valid?
+  end
+
+  test "content should be present" do
+    assert_not @todo1.valid?
+    assert_not @todo2.valid?
+  end
+
 end
